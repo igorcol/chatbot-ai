68affb3121bbcb68c86659dceb89cfe39d0032d5
igorcol <igor.colombini@gmail.com>
2025-02-17 00:17:57 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,10 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+import Chat from "@/components/Chat";
 
 
 export default function Home() {
   return (
     <div className="flex min-h-screen bg-slate-100 items-center justify-center">
-      <Card className="w-[440px] h-[600px] grid grid-rows-[min-content_1fr_min-content]">
-        <CardHeader>
-          <CardTitle>Chat AI</CardTitle>
-          <CardDescription>Using Versel SDK to create a chatbot.</CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-4">
-
-          <div className="flex gap-3 text-slate-600 text-sm">
-            <Avatar>
-              <AvatarFallback>IC</AvatarFallback>
-              <AvatarImage src="https://github.com/igorcol.png" />
-            </Avatar>
-            <p className="leading-relaxed">
-              <span className="block font-bold text-slate-700">Usuário:</span>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto illum eos aliquam voluptate enim rerum voluptatum! Quod ex, ullam explicabo nihil dolorem facere eaque in, magni minus libero nulla!
-            </p>
-          </div>
-
-          <div className="flex gap-3 text-slate-600 text-sm">
-            <Avatar>
-              <AvatarFallback>AI</AvatarFallback>
-              <AvatarImage src="https://github.com/rocketseat.png" />
-            </Avatar>
-            <p className="leading-relaxed">
-              <span className="block font-bold text-slate-700">ChatBot:</span>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto illum eos aliquam voluptate enim rerum voluptatum! Quod ex, ullam explicabo nihil dolorem facere eaque in, magni minus libero nulla!
-            </p>
-          </div>
-
-        </CardContent>
-        <CardFooter className="space-x-2">
-          <Input placeholder="How can I help you?" />
-          <Button type="submit">Send</Button>
-        </CardFooter>
-      </Card>
+      <Chat  />
     </div>
   );
 }
---
+++ src/components/Chat.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { useChat } from "@ai-sdk/react";
+
+export interface ChatProps {}
+
+export default function Chat(props: ChatProps) {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: '/api/chat'
+  });
+
+  return (
+    <Card className="w-[440px]">
+      <CardHeader>
+        <CardTitle>Chat AI</CardTitle>
+        <CardDescription>Using Versel SDK to create a chatbot.</CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <ScrollArea className="h-[400px] w-full pr-5">
+        {messages.map((message) => {
+          return (
+            <div key={message.id} className="flex gap-3 text-slate-600 text-sm mb-4">
+              {message.role === 'user' && (
+                <Avatar>
+                    <AvatarFallback>IC</AvatarFallback>
+                    <AvatarImage src="https://github.com/igorcol.png" />
+                </Avatar>
+              )}
+              {message.role === 'assistant' && (
+                <Avatar>
+                    <AvatarFallback>AI</AvatarFallback>
+                    <AvatarImage src="https://github.com/rocketseat.png" />
+                </Avatar>
+              )}
+              <p className="leading-relaxed">
+                <span className="block font-bold text-slate-700">
+                    {message.role === 'user' ? 'Usuário:' : 'ChatBot:'}
+                </span>
+                {message.content}
+              </p>
+            </div>
+          );
+        })}
+        </ScrollArea>
+      </CardContent>
+
+      <CardFooter>
+        <form className="w-full flex gap-2" onSubmit={handleSubmit}>
+          <Input
+            placeholder="Say something..."
+            value={input}
+            onChange={handleInputChange}
+          />
+          <Button type="submit">Send</Button>
+        </form>
+      </CardFooter>
+    </Card>
+  );
+}
